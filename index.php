3f21edde8cfbafbf6753c8bf143fe1b42983b844
Naveen Kumar <101378030+Nightcrawler1256@users.noreply.github.com>
2023-10-21 16:30:44 +0530

    Day 7 of Web Development Internship

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$firstname=$_POST[fname];
+$lastname=$_POST[lname];
+$nickname=$_POST[nickname];
+$email=$_POST[email];
+$password=$_POST[password];
+$gender=$_POST[gender];
+$DOB=$_POST[DOB];
+$tel=$_POST[tel];
+$address=$_POST[Add];
+
+echo $firstname;
+echo $lastname;
+
+?>
